edf64916a685b06c6d4afa5b47dda134b8c69b88
fstanisic <f.stanisic@levi9.com>
2022-09-26 09:16:03 +0200

    AotProxyHint on generate funny name

--- src/main/java/com/levi9/nativelambda/GenerateFunnyName.java
+++ src/main/java/com/levi9/nativelambda/GenerateFunnyName.java
@@ -2,11 +2,14 @@ package com.levi9.nativelambda;
 
 import com.levi9.nativelambda.dto.FunnyName;
 import org.springframework.cglib.core.internal.Function;
+import org.springframework.nativex.hint.AotProxyHint;
+import org.springframework.nativex.hint.ProxyBits;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
 @Component
+@AotProxyHint(targetClass = com.levi9.nativelambda.GenerateFunnyName.class, interfaces = {java.util.function.Function.class}, proxyFeatures = ProxyBits.IS_STATIC)
 public class GenerateFunnyName implements Function<FunnyNameRequest, FunnyName> {
     @Override
     public FunnyName apply(FunnyNameRequest funnyNameRequest) {
