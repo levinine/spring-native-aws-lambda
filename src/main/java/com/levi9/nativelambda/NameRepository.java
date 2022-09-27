0f8b7d56461b82308bf62bbb3a40faddffd89be2
fstanisic <f.stanisic@levi9.com>
2022-09-27 13:29:44 +0200

    drop uneeded config

--- src/main/java/com/levi9/nativelambda/NameRepository.java
+++ src/main/java/com/levi9/nativelambda/NameRepository.java
@@ -1,14 +1,11 @@
 package com.levi9.nativelambda;
 
 import org.springframework.stereotype.Repository;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
 import software.amazon.awssdk.enhanced.dynamodb.Key;
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.StaticTableSchema;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 import java.time.Instant;
 import java.util.Optional;
@@ -18,16 +15,7 @@ import static software.amazon.awssdk.enhanced.dynamodb.mapper.StaticAttributeTag
 @Repository
 public class NameRepository {
 
-    ProfileCredentialsProvider credentialsProvider = ProfileCredentialsProvider.create();
-    Region region = Region.EU_CENTRAL_1;
-    DynamoDbClient ddb = DynamoDbClient.builder()
-            .credentialsProvider(credentialsProvider)
-            .region(region)
-            .build();
-
-    DynamoDbEnhancedClient enhancedClient = DynamoDbEnhancedClient.builder()
-            .dynamoDbClient(ddb)
-            .build();
+    DynamoDbEnhancedClient enhancedClient = DynamoDbEnhancedClient.create();
 
 
     private final TableSchema<FunnyNameEntity> tableSchema =
--- src/main/java/com/levi9/nativelambda/NativeLambdaApplication.java
+++ src/main/java/com/levi9/nativelambda/NativeLambdaApplication.java
@@ -6,28 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.env.EnvironmentPostProcessor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.nativex.hint.SerializationHint;
 
 @SpringBootApplication
 @SerializationHint(types = {FunnyNameDto.class, GeneratedFunnyName.class})
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, // No JPA
         DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
-@Configuration
-@Import({DynamoDbConfig.class})
-public class NativeLambdaApplication implements EnvironmentPostProcessor {
-
+public class NativeLambdaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NativeLambdaApplication.class, args);
     }
 
-    @Override
-    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
-        application.setAllowBeanDefinitionOverriding(true);
-
-    }
 }
