f94060472d9165ca07eadec483f7b7911c42e5c9
fstanisic <f.stanisic@levi9.com>
2022-09-28 12:55:41 +0200

    remove mvc

--- src/main/java/com/levi9/nativelambda/NativeLambdaApplication.java
+++ src/main/java/com/levi9/nativelambda/NativeLambdaApplication.java
@@ -6,38 +6,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.nativex.hint.SerializationHint;
-import org.springframework.web.servlet.HandlerAdapter;
-import org.springframework.web.servlet.HandlerMapping;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @SpringBootApplication
 @SerializationHint(types = {FunnyNameDto.class, GeneratedFunnyName.class})
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, // No JPA
         DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @Import(FunnyNameController.class)
-@EnableWebMvc
 public class NativeLambdaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NativeLambdaApplication.class, args);
     }
 
-
-    @Bean
-    public HandlerMapping handlerMapping() {
-        return new RequestMappingHandlerMapping();
-    }
-
-    /*
-     * Create required HandlerAdapter, to avoid several default HandlerAdapter instances being created
-     */
-    @Bean
-    public HandlerAdapter handlerAdapter() {
-        return new RequestMappingHandlerAdapter();
-    }
 }
