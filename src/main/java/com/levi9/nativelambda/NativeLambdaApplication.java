b402d9fec6c3a076f62120a5200b24f417481e42
fstanisic <f.stanisic@levi9.com>
2022-09-28 09:43:13 +0200

    exclude servlet container

--- src/main/java/com/levi9/nativelambda/NativeLambdaApplication.java
+++ src/main/java/com/levi9/nativelambda/NativeLambdaApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Import;
 import org.springframework.nativex.hint.SerializationHint;
 
 @SpringBootApplication
 @SerializationHint(types = {FunnyNameDto.class, GeneratedFunnyName.class})
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, // No JPA
         DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@Import(FunnyNameController.class)
 public class NativeLambdaApplication {
 
     public static void main(String[] args) {
