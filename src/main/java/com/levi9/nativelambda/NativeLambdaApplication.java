8759a05142772aa5626b44195a3283a8030c2343
fstanisic <f.stanisic@levi9.com>
2022-09-29 11:03:01 +0200

    rewrite handler

--- src/main/java/com/levi9/nativelambda/NativeLambdaApplication.java
+++ src/main/java/com/levi9/nativelambda/NativeLambdaApplication.java
@@ -1,26 +1,73 @@
 package com.levi9.nativelambda;
 
-import org.springframework.boot.WebApplicationType;
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.nativex.hint.SerializationHint;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 @SpringBootApplication
 @SerializationHint(types = {FunnyNameDto.class, GeneratedFunnyName.class})
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, // No JPA
         DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @Import(FunnyNameController.class)
-public class NativeLambdaApplication {
+@EnableWebMvc
+public class NativeLambdaApplication extends SpringBootServletInitializer implements RequestStreamHandler {
+
+    private static Logger logger = LoggerFactory.getLogger(NativeLambdaApplication.class);
+
+    private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(NativeLambdaApplication.class)
-                .web(WebApplicationType.NONE)
-                .run(args);
+
+        try {
+            logger.info("Creating handler");
+            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(NativeLambdaApplication.class);
+            logger.info("Created handler");
+        } catch (ContainerInitializationException e) {
+            // if we fail here. We re-throw the exception to force another cold start
+            e.printStackTrace();
+            throw new RuntimeException("Could not initialize Spring Boot application", e);
+        }
     }
 
+    @Bean
+    public HandlerMapping handlerMapping() {
+        return new RequestMappingHandlerMapping();
+    }
+
+    /*
+     * Create required HandlerAdapter, to avoid several default HandlerAdapter instances being created
+     */
+    @Bean
+    public HandlerAdapter handlerAdapter() {
+        return new RequestMappingHandlerAdapter();
+    }
+
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+        handler.proxyStream(inputStream, outputStream, context);
+    }
 }
