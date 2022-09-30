8cf63d8ed4f100e9117e6422284e19183d75755f
fstanisic <f.stanisic@levi9.com>
2022-09-30 11:41:00 +0200

    handler mappings

--- src/main/java/com/levi9/nativelambda/NativeLambdaApplication.java
+++ src/main/java/com/levi9/nativelambda/NativeLambdaApplication.java
@@ -4,8 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.nativex.hint.SerializationHint;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @SpringBootApplication
 @SerializationHint(types = {FunnyNameDto.class, GeneratedFunnyName.class})
@@ -19,4 +24,18 @@ public class NativeLambdaApplication {
         SpringApplication.run(NativeLambdaApplication.class, args);
         logger.info("started app");
     }
+
+    @Bean
+    public HandlerMapping handlerMapping() {
+        return new RequestMappingHandlerMapping();
+    }
+
+    /*
+     * Create required HandlerAdapter, to avoid several default HandlerAdapter instances being created
+     */
+    @Bean
+    public HandlerAdapter handlerAdapter() {
+        return new RequestMappingHandlerAdapter();
+    }
+
 }
