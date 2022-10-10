18a85b5cc660d50b107c065255436040922ecd14
fstanisic <f.stanisic@levi9.com>
2022-10-10 10:29:33 +0200

    use message instead of aws objects

--- src/main/java/com/levi9/nativelambda/FunnyNameFunctionImplementor.java
+++ src/main/java/com/levi9/nativelambda/FunnyNameFunctionImplementor.java
@@ -1,15 +1,13 @@
 package com.levi9.nativelambda;
 
 
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
 
 import java.time.Instant;
 import java.util.*;
@@ -31,49 +29,32 @@ public class FunnyNameFunctionImplementor {
 
     private static Logger logger = LoggerFactory.getLogger(FunnyNameFunctionImplementor.class);
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
 
     @Bean
-    public Function<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> getFunnyNameById() {
+    public Function<Message<FunnyNameRequestDto>, FunnyNameEntity> getFunnyNameById() {
         return request -> {
-            try {
-                var body = request.getBody();
-                var dto = objectMapper.readValue(body, FunnyNameRequestDto.class);
-                logger.info("getFunnyNameById executed with request " + request);
-                FunnyNameEntity funnyNameEntity = repository.getName(dto.getId()).orElseThrow();
+            var dto = request.getPayload();
+            logger.info("getFunnyNameById executed with request " + request);
 
-                return RESPONSE_BUILDER
-                        .withBody(objectMapper.writeValueAsString(funnyNameEntity))
-                        .build();
-            } catch (JsonProcessingException e) {
-                throw new RuntimeException(e);
-            }
+            return repository.getName(dto.getId()).orElseThrow();
         };
     }
 
     @Bean
-    public Function<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> generateFunnyName() {
+    public Function<Message<GenerateFunnyNameDto>, String> generateFunnyName() {
         return request -> {
-            try {
-                var body = request.getBody();
-                GenerateFunnyNameDto dto = objectMapper.readValue(body, GenerateFunnyNameDto.class);
+            GenerateFunnyNameDto dto = request.getPayload();
 
-                logger.info("generateFunnyName executed with request " + dto);
-                repository.saveName(
-                        FunnyNameEntity.builder()
-                                .id(UUID.randomUUID().toString())
-                                .name(namePrefixes.get(randomizer.nextInt(namePrefixes.size())) + " " + dto.getName())
-                                .generatedAt(Instant.now())
-                                .build()
+            logger.info("generateFunnyName executed with request " + dto);
+            repository.saveName(
+                    FunnyNameEntity.builder()
+                            .id(UUID.randomUUID().toString())
+                            .name(namePrefixes.get(randomizer.nextInt(namePrefixes.size())) + " " + dto.getName())
+                            .generatedAt(Instant.now())
+                            .build()
 
-                );
-                return RESPONSE_BUILDER
-                        .withBody("") //todo
-                        .build();
-            } catch (JsonProcessingException e) {
-                throw new RuntimeException(e);
-            }
+            );
+            return "";
         };
     }
 }
