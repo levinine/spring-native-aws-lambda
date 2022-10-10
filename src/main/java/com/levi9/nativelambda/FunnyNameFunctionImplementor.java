d15c4a1dc099dc11db475f84c49e9786418f58e0
fstanisic <f.stanisic@levi9.com>
2022-10-10 14:28:03 +0200

    drop uneeded builder

--- src/main/java/com/levi9/nativelambda/FunnyNameFunctionImplementor.java
+++ src/main/java/com/levi9/nativelambda/FunnyNameFunctionImplementor.java
@@ -1,7 +1,6 @@
 package com.levi9.nativelambda;
 
 
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,19 +9,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
 
 import java.time.Instant;
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
 import java.util.function.Function;
 
 @Configuration
 public class FunnyNameFunctionImplementor {
 
     final static List<String> namePrefixes = Arrays.asList("Huckleberry", "Sizzle Pants", "Itsy Bitsy", "Speedy", "Mighty", "Legendary", "Jumpy");
-    public static final APIGatewayV2HTTPResponse.APIGatewayV2HTTPResponseBuilder RESPONSE_BUILDER = APIGatewayV2HTTPResponse.builder()
-            .withStatusCode(200)
-            .withIsBase64Encoded(false)
-            .withHeaders(Map.of(
-                    "Content-Type", "application/json"
-            ));
     Random randomizer = new Random();
     @Autowired
     NameRepository repository;
